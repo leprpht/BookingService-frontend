43b27ca40095ec6869a23936896cb0130afd3aac
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2026-03-26 10:27:54 +0100

    Initialize auth components and services

---
+++ src/app/components/auth/login/login.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'booking-service-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login {
+
+}
---
+++ src/app/components/auth/registration/registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'booking-service-registration',
+  imports: [],
+  templateUrl: './registration.html',
+  styleUrl: './registration.scss',
+})
+export class Registration {
+
+}
