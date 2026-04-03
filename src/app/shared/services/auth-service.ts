18cc0c7ee2c7818cc85728a8b3035bb90bd9d386
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2026-04-03 12:55:25 +0200

    Implement profile menu in header and refactor AuthService for shared usage

--- src/app/shared/services/auth-service.ts
+++ src/app/shared/services/auth-service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { UserStateService } from './user-state-service';
 
 const API_URL = `${environment.apiUrl}/api/User/auth`;
 
@@ -14,6 +15,7 @@ interface AuthRequest {
 })
 export class AuthService {
   private http: HttpClient = inject(HttpClient);
+  private userState = inject(UserStateService);
 
   login(email: string, password: string) {
     const request: AuthRequest = { email, password };
@@ -23,5 +25,13 @@ export class AuthService {
   register(email: string, password: string) {
     const request: AuthRequest = { email, password };
     return this.http.post(`${API_URL}/register`, request, { withCredentials: true });
+  }
+
+  logout() {
+    this.http.post(`${API_URL}/revoke`, {}, { withCredentials: true }).subscribe(() => {
+      this.userState.user.set(null);
+      this.userState.isAuthenticated.set(false);
+      window.location.reload();
+    });
   }
 }
